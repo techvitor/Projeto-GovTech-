5f924030812dbf8439bf416e7ccb370172c98b0d
techvitor <vitorftn4@gmail.com>
2026-04-10 00:07:06 -0300

    Classe HistoricoStatus e Enum Prioridade / Status

---
+++ src/HistoricoStatus.java
@@ -0,0 +1,20 @@
+import java.time.LocalDateTime;
+
+public class HistoricoStatus {
+    private Status status;
+    private LocalDateTime data;
+    private String responsavel;
+    private String comentario;
+
+    public HistoricoStatus(Status status, String responsavel, String comentario) {
+        this.status = status;
+        this.data = LocalDateTime.now();
+        this.responsavel = responsavel;
+        this.comentario = comentario;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + data + "] Status: " + status + " | Por: " + responsavel + " | Obs: " + comentario;
+    }
+}
---
+++ src/Prioridade.java
@@ -0,0 +1,6 @@
+public enum Prioridade {
+    BAIXA,
+    MEDIA,
+    ALTA,
+    URGENTE
+}
